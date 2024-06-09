1af64474d3949662175410ce6285aa035b04f33b
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-06-09 01:48:41 -0600

    creacion de nav bar footer y header de la aplicacion web

---
+++ vistas/templates/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar fixed-top navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../../vistas/paciente/index.php">
+      <img src="../../images/icono.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top me-2">
+      PACIENTES
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../../vistas/cliente/index.php">
+            <i class="bi bi-house-door-fill me-2"></i>INICIO
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-fill me-2"></i>PACIENTES
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="../../vistas/paciente/index.php"><i class="bi bi-plus-circle me-2"></i>CREAR</a></li>
+            <li><a class="dropdown-item" href="../paciente/buscar.php"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
